101bedfffc5f0f16b9f5bed34c4c6705988e6b42
Dasha Gurska <90865882+Meczta@users.noreply.github.com>
2021-12-09 00:28:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello from blyatDasha!'); //write a response to the client
+  res.write('Hello from blyaaaaaDasha!'); //write a response to the client
   res.end(); //end the response
 }).listen(3000); //the server object listens on port 8080
